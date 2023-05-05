dba437cee7084725bcc617628a636c23215c5f5f
ljj <120035959@qq.com>
2023-05-05 17:25:37 +0800

    update example

--- example/timer.cpp
+++ example/timer.cpp
@@ -6,12 +6,25 @@ using namespace coplus;
 
 task<> client_test() {
     while (true) {
-        std::cout << "wait for 1000ms\n";
         co_await 1000_ms;
+        std::cout << "wait for 1000ms\n";
     }
 }
-
+task<> client_test1() {
+    while (true) {
+        co_await 2000_ms;
+        std::cout << "wait for 2000ms\n";
+    }
+}
+task<> client_test2() {
+    while (true) {
+        co_await 3000_ms;
+        std::cout << "wait for 3000ms\n";
+    }
+}
 int main() {
     co_runtime::spawn(client_test());
+    co_runtime::spawn(client_test1());
+    co_runtime::spawn(client_test2());
     co_runtime::run();
 }
